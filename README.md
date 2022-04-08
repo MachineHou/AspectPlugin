c02de71414fb69154730ffd97889e6a77a3884ca
侯杰 <houjie@fosung.com>
2022-04-08 14:24:03 +0800

    aop提供跨modeule的临时解决方案

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # AspectPlugin
-aspect 自动集成插件
+aspect 自动集成插件（适用于 Android Studio 环境下，其它环境下未测试）
+
+[![Release Version](https://img.shields.io/badge/release-2.4-green.svg)](https://github.com/2017398956/AspectPlugin/releases)
+ 
+## 更新说明
+
+#### 2.4 解决 Linux 和 mac 下编译出错的问题
+#### 2.3 解决 java 和 kotlin 混编时的缓存问题
+#### 2.0 支持 java 和 kotlin 混编
+
+## 使用方法
+
+在根目录的 build 中加入如下代码
+
+    buildscript {
+        repositories {
+           ...
+           maven { url 'https://jitpack.io' }
+        }
+        dependencies {
+            ...
+            classpath 'com.github.2017398956:AspectPlugin:2.4'
+        }
+    }
+
+
+在需要的 module 中添加如下代码
+
+
+    apply plugin: 'AspectPlugin'
+    dependencies { api 'org.aspectj:aspectjrt:1.9.6'}
+    
+另外，如果使用了 kotlin 要把 apply plugin: 'AspectPlugin' 放在 apply plugin: 'kotlin-xxx' 后面
+
+**注意** ：进行 AOP 操作的文件和 AOP 操作的对象要在一个 module 中。
+
+关于跨 module 操作、 对 jar 或 aar 进行 aop 操作，等我有空再支持吧。
+
+这里提供一个临时解决跨 module 的方案，例如：你需要在 B module 中对 A module 的 LogXXX.java 进行 aop 操作，那么你可以在 A module 中 写一个 AspForLogxxxx.java 的文件，并在这个 AspForLogxxxx.java 添加一个 listener （或者 一个 listener 集合）；然后在 B moudle 中 通过实例化这个 listener 或者往这个集合中添加 listener 的方式来绕过跨 module 的问题。
+    
+    
+一个简单的 Demo ： [AspectPluginDemo](https://github.com/2017398956/AspectPluginDemo)
+
+应用示例：[AbcPermission](https://github.com/2017398956/AbcPermission "AbcPermission") 
---
+++ src/main/groovy/personal/nfl/aspect/plugin/AspectPlugin.groovy
@@ -0,0 +1,165 @@
+package personal.nfl.aspect.plugin
+
+import org.aspectj.bridge.IMessage
+import org.aspectj.bridge.MessageHandler
+import org.aspectj.tools.ajc.Main
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.Task
+import personal.nfl.aspect.plugin.utils.FileUtil
+
+/**
+ * Created by 2017398956 on 2017/12/25.
+ */
+class AspectPlugin implements Plugin<Project> {
+
+    private String sourceJDK = "1.8"
+    private String targetJDK = "1.8"
+    private File aopTemp
+
+    void apply(Project project) {
+
+        // def hasApp = project.plugins.withType(AppPlugin)  //判断是否是主module
+        // def hasLib = project.plugins.withType(LibraryPlugin)//判断是否是library
+        // final def variants
+        // if (hasApp) {
+        //      variants = project.android.applicationVariants
+        // } else {
+        //      variants = project.android.libraryVariants
+        // }
+        //
+        // project.dependencies {
+        //      compile 'org.aspectj:aspectjrt:1.8.10'
+        // }
+        //
+        // project.extensions.create('aspect')
+
+        aopTemp = new File(project.buildFile.parentFile.absolutePath + "${File.separator}src${File.separator}main${File.separator}java${File.separator}AopTemp.java")
+        FileUtil.delete(aopTemp)
+
+        if (project.plugins.findPlugin("kotlin-android") != null) {
+            project.kotlin.target.compilations.all { variant ->
+                Task kotlinCompile = variant.getCompileKotlinTaskProvider().get()
+                if ("release" == variant.name || "debug" == variant.name) {
+                    kotlinCompile.doLast {
+                        println("kolinCompile.path : " + kotlinCompile.path)
+                        println("This is print after kotlin compile and should exc aop .")
+                        FileWriter fileWriter = new FileWriter(aopTemp)
+                        fileWriter.write("public class AopTemp {int a = " + new Random().nextInt() + ";}")
+                        fileWriter.flush()
+                        fileWriter.close()
+                    }
+                }
+            }
+        }
+
+        if (project.hasProperty('android') && project.android != null) {
+            if (project.android.hasProperty('applicationVariants')
+                    && project.android.applicationVariants != null) {
+                project.android.applicationVariants.all { variant ->
+                    // WARNING: API 'variant.getJavaCompiler()' is obsolete and
+                    // has been replaced with 'variant.getJavaCompileProvider()'.
+                    // doLast(variant.getJavaCompiler())
+                    doFirst(variant.getJavaCompileProvider().get())
+                    doLast(variant.getJavaCompileProvider().get())
+                }
+            }
+            if (project.android.hasProperty('libraryVariants')
+                    && project.android.libraryVariants != null) {
+                project.android.libraryVariants.all { variant ->
+                    doFirst(variant.getJavaCompileProvider().get())
+                    doLast(variant.getJavaCompileProvider().get())
+                }
+            }
+        }
+    }
+
+    private void doFirst(Task javaCompile) {
+        javaCompile.doFirst {
+            if (project.hasProperty('android') && project.android != null) {
+                if (project.android.hasProperty('compileOptions') && project.android.compileOptions != null) {
+                    if (project.android.compileOptions.hasProperty('targetCompatibility') && project.android.compileOptions.targetCompatibility != null) {
+                        targetJDK = project.android.compileOptions.properties.get('targetCompatibility')
+                    }
+                    if (project.android.compileOptions.hasProperty('sourceCompatibility') && project.android.compileOptions.sourceCompatibility != null) {
+                        sourceJDK = project.android.compileOptions.properties.get('sourceCompatibility')
+                    }
+                }
+            }
+        }
+    }
+
+    private void doLast(Task javaCompile) {
+        javaCompile.doLast {
+
+            MessageHandler handler = new MessageHandler(true)
+            String aspectPath = javaCompile.classpath.asPath
+            String inPath = javaCompile.destinationDir.toString()
+            String dPath = javaCompile.destinationDir.toString();
+            String classpath = javaCompile.classpath.asPath
+
+            if (null != aopTemp && aopTemp.exists()) {
+                println("delete file:" + aopTemp.absolutePath)
+                FileUtil.delete(aopTemp)
+            }
+            File aopTempClass = new File(dPath + File.separator + "AopTemp.class")
+            if (aopTempClass.exists()) {
+                println("delete file:" + aopTempClass.absolutePath)
+                FileUtil.delete(aopTempClass)
+            }
+
+            // 配置 kotlin 相关参数
+            String kotlinInPath = ""
+            if (dPath.contains("debug${File.separator}classes")) {
+                kotlinInPath = javaCompile.temporaryDir.getParentFile().path + "${File.separator}kotlin-classes${File.separator}debug"
+            } else {
+                kotlinInPath = javaCompile.temporaryDir.getParentFile().path + "${File.separator}kotlin-classes${File.separator}release"
+            }
+            String pathSplit = ";"
+            if (kotlinInPath.startsWith("/")){
+                // 说明是 linux 或者 mac 环境
+                pathSplit = ":"
+            }
+            // java 的 class 文件实现 aop
+            String[] javacArgs = ["-showWeaveInfo",
+                                  "-source", sourceJDK,
+                                  "-target", targetJDK,
+                                  "-inpath", kotlinInPath + pathSplit + inPath,
+                                  "-aspectpath", aspectPath,
+                                  "-d", dPath,
+                                  "-classpath", classpath,
+                                  "-bootclasspath", project.android.bootClasspath.join(File.pathSeparator)]
+            new Main().run(javacArgs, handler)
+            File[] kotlinClassFiles = FileUtil.listFiles(kotlinInPath, true)
+            File javacKotlinFile
+            for (File temp : kotlinClassFiles) {
+                if (temp.isFile() && temp.getName().endsWith(".class")) {
+                    javacKotlinFile = new File(inPath + File.separator + temp.absolutePath.replace(kotlinInPath, ""))
+                    if (null != javacKotlinFile && javacKotlinFile.exists()) {
+                        FileUtil.delete(temp)
+                        FileUtil.copyFile(javacKotlinFile, temp)
+                        FileUtil.delete(javacKotlinFile)
+                    }
+                }
+            }
+
+            def log = project.logger
+            for (IMessage message : handler.getMessages(null, true)) {
+                switch (message.getKind()) {
+                    case IMessage.ABORT:
+                    case IMessage.ERROR:
+                    case IMessage.FAIL:
+                        log.error message.message, message.thrown
+                        break;
+                    case IMessage.WARNING:
+                    case IMessage.INFO:
+                        log.info message.message, message.thrown
+                        break;
+                    case IMessage.DEBUG:
+                        log.debug message.message, message.thrown
+                        break;
+                }
+            }
+        }
+    }
+}
